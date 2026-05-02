b27ade4c6363dd109ab3602c14a0b3e150ba568c
kelvinquartey <kelvinquartey1@gmail.com>
2026-05-02 00:25:56 +0000

    feat: add root layout and scaffold core UI components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import Header from '@/components/Header'
+import MobileNavigation from '@/components/MobileNavigation'
+import Sidebar from '@/components/Sidebar'
+import { getCurrentUser } from '@/lib/actions/user.actions'
+import { redirect } from "next/navigation";
+
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}){
+    const user = await getCurrentUser()
+
+    if(!user) return redirect("/sign-in");
+
+  return (
+    <main className="flex min-h-screen bg-[var(--card)]">
+        <Sidebar user={user}/>
+
+        <section className="flex h-full flex-1 flex-col">
+            <MobileNavigation />
+            <Header />
+
+            <div className="main-content remove-scrollbar">{children}</div>
+        </section>
+    </main>
+  )
+}
+
