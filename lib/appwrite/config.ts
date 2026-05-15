f02bb234132bc69ac76753a6cdd7446348e4a5e7
kelvinquartey <kelvinquartey1@gmail.com>
2026-05-15 01:14:01 +0000

    fix: make Appwrite bucket env variable public

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -4,6 +4,6 @@ export const appwriteConfig = {
     databaseId: process.env.APPWRITE_DATABASE!,
     filesTableId: process.env.APPWRITE_FILES_TABLE!,
     userTableId: process.env.APPWRITE_USERS_TABLE!,
-    bucketId :process.env.APPWRITE_BUCKET!,
+    bucketId :process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
     secretKey: process.env.APPWRITE_SECRET!,
 }
