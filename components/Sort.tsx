a68c7edfb87837d136826dd5f1fbc340064c3b79
kelvinquartey <kelvinquartey1@gmail.com>
2026-05-21 18:44:52 +0000

    feat: implement sort dropdown

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+"use client"
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
 
 const Sort = () => {
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="shad-no-focus sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            className="shad-select-item"
+            value={sort.value}
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   )
 }
 
