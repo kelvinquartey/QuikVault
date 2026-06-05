67ee3b6982e2a29fe0dbdd71ec567fb859bb2d77
kelvinquartey <kelvinquartey1@gmail.com>
2026-06-05 01:03:34 +0000

    style: improve file grid responsiveness and card sizing

--- components/Card.tsx
+++ components/Card.tsx
@@ -24,7 +24,7 @@ const Card = ({ file }: {file: FileDocument}) => {
         <div className="flex flex-col items-end justify-between">
           <ActionDropdown file={file} />
 
-          <p className="body-1">{convertFileSize(file.size)}</p>
+          <p className="file-size-pill">{convertFileSize(file.size)}</p>
         </div>
 
       </div>
